4947a4b325d76d7730cd226e0d4f1dd4e15d69d6
Uriel Rosario <urielrre@gmail.com>
2025-01-20 14:24:54 -0400

    Actualizando caja y core

---
+++ Caja/Usuario/PagUsuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caja
+{
+    public partial class PagUsuario : Form
+    {
+        public PagUsuario()
+        {
+            InitializeComponent();
+            lblDateTime.Text = DateTime.Now.ToString();
+        }
+
+        private void btnAgregarProducto_Click(object sender, EventArgs e)
+        {
+            if(cbListaProductos.SelectedItem != null)
+            {
+                string product = cbListaProductos.SelectedItem.ToString();
+                lbFactura.Items.Add(product);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDateTime.Text = DateTime.Now.ToString();
+        }
+    }
+}
